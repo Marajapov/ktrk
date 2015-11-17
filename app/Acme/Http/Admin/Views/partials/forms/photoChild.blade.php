d8ab1fec205bfd8091e09c06387470cebce09dcf
Marajapov <marajapovabakan@mail.ru>
2015-11-17 13:56:13 +0600

    abakan's changes

--- app/Acme/Http/Admin/Views/partials/forms/photoChild.blade.php
+++ app/Acme/Http/Admin/Views/partials/forms/photoChild.blade.php
@@ -35,6 +35,20 @@
         </div>
     </div>
 
+    <div class="col-sm-8">
+        <div class="form-group">
+            <label class="control-label">Таг на кыргызском</label>
+            {!! Form::text('photoTag', null, ["class" => "form-control", "required" => true, "title" => ""]) !!}
+        </div>
+    </div>
+
+    <div class="col-sm-8">
+        <div class="form-group">
+            <label class="control-label">Таг на русском</label>
+            {!! Form::text('photoTagRu', null, ["class" => "form-control", "required" => true, "title" => ""]) !!}
+        </div>
+    </div>
+
 <!--<div class="col-xs-12">
         <div class="form-group">
             <label class="control-label">Файл</label>
@@ -45,6 +59,7 @@
 
 
     <div class="col-sm-8">
+        <label for="file" class="control-label">Фото</label>
         <div class="fileinput fileinput-new input-group" data-provides="fileinput">
           <div class="form-control" data-trigger="fileinput"><i class="glyphicon glyphicon-file fileinput-exists"></i> <span class="fileinput-filename"></span></div>
           <span class="input-group-addon btn btn-default btn-file"><span class="fileinput-new">{{ trans('site.AdminBannerSelectImg') }}</span><span class="fileinput-exists">{{ trans('site.Change') }}</span>
