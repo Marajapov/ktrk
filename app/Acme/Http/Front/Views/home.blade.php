499d92201e7cfbd61c787d246359ce6297ccb70c
Marajapov <marajapovabakan@mail.ru>
2015-12-10 17:54:36 +0600

    abakan working on home blade

--- app/Acme/Http/Front/Views/home.blade.php
+++ app/Acme/Http/Front/Views/home.blade.php
@@ -466,7 +466,12 @@
                   <div class="carousel-slick">
                     @if($images != 1)
                       @foreach($images as $image)
-                        <div class="col-md-4"><a href="#"><img src="{{ asset('froala/uploads/'.$image->name) }}" alt=""/><span>Название</span><div class="overlay"></div></a></div>
+                        <div class="col-md-4">
+                          <a href="#">
+                            <img src="{{ asset('froala/uploads/'.$image->name) }}" alt=""/><span>Название</span>
+                            <div class="overlay"></div>
+                          </a>
+                        </div>
                       @endforeach
                     @endif
                   </div>
