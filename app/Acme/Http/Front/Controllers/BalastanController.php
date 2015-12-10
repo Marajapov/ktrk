43a8b7ac702088a2746844199580019a7abe331e
Marajapov <marajapovabakan@mail.ru>
2015-12-10 17:03:34 +0600

    abakan

--- app/Acme/Http/Front/Controllers/BalastanController.php
+++ app/Acme/Http/Front/Controllers/BalastanController.php
@@ -25,6 +25,36 @@ class BalastanController extends Controller
 
         return view('Front::channel.balastan.posts', ['channel' => $channel]);
     }
+  public function about()
+    {
+        $channel = \Model\Channel\ModelName::name('balastan')->first();
+        $backgroundMain = \Model\Background\ModelName::where('published','=',true)->first();
 
+        return view('Front::channel.balastan.about', [
+            'channel' => $channel,
+            'backgroundMain' => $backgroundMain,
+            ]);
+    }
+
+    public function world()
+    {
+        $channel = \Model\Channel\ModelName::name('balastan')->first();
+        $backgroundMain = \Model\Background\ModelName::where('published','=',true)->first();
+
+        return view('Front::channel.balastan.world', [
+            'channel' => $channel,
+            'backgroundMain' => $backgroundMain,
+            ]);
+    }
+    public function photos()
+    {
+        $channel = \Model\Channel\ModelName::name('balastan')->first();
+        $backgroundMain = \Model\Background\ModelName::where('published','=',true)->first();
+
+        return view('Front::channel.balastan.photos', [
+            'channel' => $channel,
+            'backgroundMain' => $backgroundMain,
+            ]);
+    }
 
 }
