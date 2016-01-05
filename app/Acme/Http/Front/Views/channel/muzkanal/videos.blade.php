509adc598e20e5ab344d22f9c067e8cc875f4e81
Eldos Kadyrov <eldos.kadyrov@gmail.com>
2016-01-05 14:54:42 +0600

    Music Exclsuve and etc.

--- app/Acme/Http/Front/Views/channel/muzkanal/videos.blade.php
+++ app/Acme/Http/Front/Views/channel/muzkanal/videos.blade.php
@@ -142,25 +142,25 @@
             <div class="row topvideos videostyle">
                <div class="panel panel-default">
                   <div class="panel-heading">
-                     <h3 class="panel-title"><span>{{ trans('radiopages.AllVideos') }}</span></h3>
+                     <h3 class="panel-title"><span>{{ trans('radiopages.NewClips') }}</span></h3>
                   </div>
                   <div class="panel-body">
                      <div class="col-md-12 videofix">
                         <div class="row" style="margin:-11px;">
-                           @if($postAll)
-                           @foreach($postAll as $allVideo)   
+                           @if($MediaNew)
+                           @foreach($MediaNew as $newVideo)   
                            <div class="col-md-3 muzvideomain col-sm-6 col-xs-12">
                               <div class="muzvideoall">
-                                 <a href="{{ route('muzkanal.video', $allVideo)}}">
-                                 <img src="http://img.youtube.com/vi/{{ $allVideo->getUrl()}}/mqdefault.jpg" alt=""/></a>
+                                 <a href="{{ route('muzkanal.video', $newVideo)}}">
+                                 <img src="http://img.youtube.com/vi/{{ $newVideo->getUrl()}}/mqdefault.jpg" alt=""/></a>
                                  <div class="item-desc">
                                     <ul>
-                                       <a href="{{ route('muzkanal.video', $allVideo)}}">
-                                          <li class="item-artist">{{ $allVideo->getName() }}</li>
+                                       <a href="{{ route('muzkanal.video', $newVideo)}}">
+                                          <li class="item-artist">{{ $newVideo->getName() }}</li>
                                        </a>
                                     </ul>
                                  </div>
-                                 <div class="views"><i class="fa fa-eye"></i>{{ $allVideo->getViewed() }}</div>
+                                 <div class="views"><i class="fa fa-eye"></i>{{ $newVideo->getViewed() }}</div>
                               </div>
                            </div>
                            @endforeach
@@ -169,21 +169,21 @@
                         <nav class="muzpaginate">
                            <ul class="pagination">
                               <li>
-                                 <a href="{{ route('muzkanal.videos', ['page' => 1]) }}" class="btn btn-default @if($postAll->currentPage() == 1) disabled @endif">{{ trans('site.Start') }}</a>
+                                 <a href="{{ route('muzkanal.videos', ['page' => 1]) }}" class="btn btn-default @if($postAllNew->currentPage() == 1) disabled @endif">{{ trans('site.Start') }}</a>
                               </li>
                               <li>
-                                 <a href="{{ $postAll->previousPageUrl() }}" class="btn btn-default"><span class="glyphicon glyphicon-chevron-left"></span></a>
+                                 <a href="{{ $postAllNew->previousPageUrl() }}" class="btn btn-default"><span class="glyphicon glyphicon-chevron-left"></span></a>
                               </li>
                               <li>
-                                 <a href="{{ $postAll->nextPageUrl() }}" class="btn btn-default"><span class="glyphicon glyphicon-chevron-right"></span></a>
+                                 <a href="{{ $postAllNew->nextPageUrl() }}" class="btn btn-default"><span class="glyphicon glyphicon-chevron-right"></span></a>
                               </li>
-                              @for($i = 0, $j = 1; $i < $postAll->total(); $i+=$perPage)
+                              @for($i = 0, $j = 1; $i < $postAllNew->total(); $i+=$perPage)
                               <li>
-                                 <a href="{{ route('muzkanal.videos', ['page' => $j]) }}" class="btn btn-default @if($postAll->currentPage() == $j) active @endif">{{ $j++ }}</a>
+                                 <a href="{{ route('muzkanal.videos', ['page' => $j]) }}" class="btn btn-default @if($postAllNew->currentPage() == $j) active @endif">{{ $j++ }}</a>
                               </li>
                               @endfor
                               <li>
-                                 <a href="{{ route('muzkanal.videos', ['page' => ceil($postAll->total()/$perPage)]) }}" class="btn btn-default @if($postAll->currentPage() == ceil($postAll->total()/$perPage)) disabled @endif">{{ trans('site.End') }}</a>
+                                 <a href="{{ route('muzkanal.videos', ['page' => ceil($postAllNew->total()/$perPage)]) }}" class="btn btn-default @if($postAllNew->currentPage() == ceil($postAllNew->total()/$perPage)) disabled @endif">{{ trans('site.End') }}</a>
                               </li>
                            </ul>
                         </nav>
@@ -192,6 +192,114 @@
                </div>
             </div>
          </div>
+         <div class="col-md-12">
+            <div class="row topvideos videostyle">
+               <div class="panel panel-default">
+                  <div class="panel-heading">
+                     <h3 class="panel-title"><span>{{ trans('radiopages.TopCLips') }}</span></h3>
+                  </div>
+                  <div class="panel-body">
+                     <div class="col-md-12 videofix">
+                        <div class="row" style="margin:-11px;">
+                           @if($MediaTop)
+                           @foreach($MediaTop as $topVideo)   
+                           <div class="col-md-3 muzvideomain col-sm-6 col-xs-12">
+                              <div class="muzvideoall">
+                                 <a href="{{ route('muzkanal.video', $topVideo)}}">
+                                 <img src="http://img.youtube.com/vi/{{ $topVideo->getUrl()}}/mqdefault.jpg" alt=""/></a>
+                                 <div class="item-desc">
+                                    <ul>
+                                       <a href="{{ route('muzkanal.video', $topVideo)}}">
+                                          <li class="item-artist">{{ $topVideo->getName() }}</li>
+                                       </a>
+                                    </ul>
+                                 </div>
+                                 <div class="views"><i class="fa fa-eye"></i>{{ $topVideo->getViewed() }}</div>
+                              </div>
+                           </div>
+                           @endforeach
+                           @endif                            
+                        </div>
+                        <nav class="muzpaginate">
+                           <ul class="pagination">
+                              <li>
+                                 <a href="{{ route('muzkanal.videos', ['page' => 1]) }}" class="btn btn-default @if($postAllTop->currentPage() == 1) disabled @endif">{{ trans('site.Start') }}</a>
+                              </li>
+                              <li>
+                                 <a href="{{ $postAllTop->previousPageUrl() }}" class="btn btn-default"><span class="glyphicon glyphicon-chevron-left"></span></a>
+                              </li>
+                              <li>
+                                 <a href="{{ $postAllTop->nextPageUrl() }}" class="btn btn-default"><span class="glyphicon glyphicon-chevron-right"></span></a>
+                              </li>
+                              @for($i = 0, $j = 1; $i < $postAllTop->total(); $i+=$perPage)
+                              <li>
+                                 <a href="{{ route('muzkanal.videos', ['page' => $j]) }}" class="btn btn-default @if($postAllTop->currentPage() == $j) active @endif">{{ $j++ }}</a>
+                              </li>
+                              @endfor
+                              <li>
+                                 <a href="{{ route('muzkanal.videos', ['page' => ceil($postAllTop->total()/$perPage)]) }}" class="btn btn-default @if($postAllTop->currentPage() == ceil($postAllTop->total()/$perPage)) disabled @endif">{{ trans('site.End') }}</a>
+                              </li>
+                           </ul>
+                        </nav>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-12">
+            <div class="row topvideos videostyle">
+               <div class="panel panel-default">
+                  <div class="panel-heading">
+                     <h3 class="panel-title"><span>Экслюзив</span></h3>
+                  </div>
+                  <div class="panel-body">
+                     <div class="col-md-12 videofix">
+                        <div class="row" style="margin:-11px;">
+                           @if($MediaLive)
+                           @foreach($MediaLive as $liveVideo)   
+                           <div class="col-md-3 muzvideomain col-sm-6 col-xs-12">
+                              <div class="muzvideoall">
+                                 <a href="{{ route('muzkanal.video', $liveVideo)}}">
+                                 <img src="http://img.youtube.com/vi/{{ $liveVideo->getUrl()}}/mqdefault.jpg" alt=""/></a>
+                                 <div class="item-desc">
+                                    <ul>
+                                       <a href="{{ route('muzkanal.video', $liveVideo)}}">
+                                          <li class="item-artist">{{ $liveVideo->getName() }}</li>
+                                       </a>
+                                    </ul>
+                                 </div>
+                                 <div class="views"><i class="fa fa-eye"></i>{{ $liveVideo->getViewed() }}</div>
+                              </div>
+                           </div>
+                           @endforeach
+                           @endif                            
+                        </div>
+                        <nav class="muzpaginate">
+                           <ul class="pagination">
+                              <li>
+                                 <a href="{{ route('muzkanal.videos', ['page' => 1]) }}" class="btn btn-default @if($postAllLive->currentPage() == 1) disabled @endif">{{ trans('site.Start') }}</a>
+                              </li>
+                              <li>
+                                 <a href="{{ $postAllLive->previousPageUrl() }}" class="btn btn-default"><span class="glyphicon glyphicon-chevron-left"></span></a>
+                              </li>
+                              <li>
+                                 <a href="{{ $postAllLive->nextPageUrl() }}" class="btn btn-default"><span class="glyphicon glyphicon-chevron-right"></span></a>
+                              </li>
+                              @for($i = 0, $j = 1; $i < $postAllLive->total(); $i+=$perPage)
+                              <li>
+                                 <a href="{{ route('muzkanal.videos', ['page' => $j]) }}" class="btn btn-default @if($postAllLive->currentPage() == $j) active @endif">{{ $j++ }}</a>
+                              </li>
+                              @endfor
+                              <li>
+                                 <a href="{{ route('muzkanal.videos', ['page' => ceil($postAllLive->total()/$perPage)]) }}" class="btn btn-default @if($postAllLive->currentPage() == ceil($postAllLive->total()/$perPage)) disabled @endif">{{ trans('site.End') }}</a>
+                              </li>
+                           </ul>
+                        </nav>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>         
       </div>
    </div>
    @stop
