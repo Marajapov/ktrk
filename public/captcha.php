3664b58b3d3f40afca51689665e1fb65d9a458a8
Aibek Kendirbaev <kendirbaev.aibek@gmail.com>
2016-01-12 18:31:30 +0600

    comments

--- public/captcha.php
+++ public/captcha.php
@@ -1,14 +1,16 @@
 <?php
 
-if(isset($_POST['author'])){
-    $r = new HttpRequest('https://www.google.com/recaptcha/api/siteverify?secret=6LcBGBUTAAAAAIMMSwe-ebmOhgMO3c-vmXn_Yyfb&response='.$_POST['g-recaptcha-response'], HttpRequest::METH_POST);
-//    $r->setOptions(array('cookies' => array('lang' => 'ru')));
+echo $_POST['g-recaptcha-response'];
 
-    if($r){
-        echo "success";
-    }else{
-        echo "error";
-    }
-}
+//if(isset($_POST['author'])){
+//    $r = new HttpRequest('https://www.google.com/recaptcha/api/siteverify?secret=6LcBGBUTAAAAAIMMSwe-ebmOhgMO3c-vmXn_Yyfb&response='.$_POST['g-recaptcha-response'], HttpRequest::METH_POST);
+//    $r->setOptions(array('cookies' => array('lang' => 'ru')));
+//
+//    if($r){
+//        echo "success";
+//    }else{
+//        echo "error";
+//    }
+//}
 
 ?>
