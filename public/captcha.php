c2725633299b6510e40dd58b4d509bedebec9c14
Aibek Kendirbaev <kendirbaev.aibek@gmail.com>
2016-01-12 18:26:49 +0600

    comments

--- public/captcha.php
+++ public/captcha.php
@@ -2,11 +2,12 @@
 
 if(isset($_POST['author'])){
     $r = new HttpRequest('https://www.google.com/recaptcha/api/siteverify?secret=6LcBGBUTAAAAAIMMSwe-ebmOhgMO3c-vmXn_Yyfb&response='.$_POST['g-recaptcha-response'], HttpRequest::METH_POST);
-    $r->setOptions(array('cookies' => array('lang' => 'ru')));
-    try {
-        echo $r->send()->getBody();
-    } catch (HttpException $ex) {
-        echo $ex;
+//    $r->setOptions(array('cookies' => array('lang' => 'ru')));
+
+    if($r){
+        echo "success";
+    }else{
+        echo "error";
     }
 }
 
