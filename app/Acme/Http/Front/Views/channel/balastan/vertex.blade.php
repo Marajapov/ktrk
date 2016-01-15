8c821ca147d0a7b0978320c080186b902b09c3f1
Eldos Kadyrov <eldos.kadyrov@gmail.com>
2016-01-15 14:01:48 +0600

    Balastan redesign

--- app/Acme/Http/Front/Views/channel/balastan/vertex.blade.php
+++ app/Acme/Http/Front/Views/channel/balastan/vertex.blade.php
@@ -54,157 +54,237 @@
    </div>
    <!--END -->
    <!-- BEGIN Home video -->
-   <div class="container">
-      <div class="row">
-         <div class="col-md-12">
-            <nav class="kidstopheader">
-               <ul>
-                  <li><a class="active" href="{{ route('balastan.home') }}"><i class="fa fa-home i1"></i>{{ trans('radiopages.Home') }}</a></li>
-                  <li><a href="{{ route('balastan.about') }}"><i class="fa fa-hand-o-up i2"></i>{{ trans('radiopages.BAbout') }}</a></li>
-                  <li><a href="{{ route('balastan.world') }}"><i class="fa fa-star i3"></i>{{ trans('radiopages.Bworld') }}</a></li>
-                  <li><a href="{{ route('balastan.video') }}"><i class="fa fa-play i4 "></i>Видео</a></li>
-                  <li><a href="{{ route('balastan.allphotos') }}"><i class="fa fa-picture-o i5"></i>{{ trans('radiopages.Photos') }}</a></li>
-                  <li><a href="#"><i class="fa fa-search i6"></i></a></li>
-               </ul>
-            </nav>
-         </div>
-      </div>
-      <div class="row firsteye">
-         <div class="col-md-8 homevideo">
-            @if($firstMedia)
-            <h2 class="homevideocat"><img src="{{asset('images/channels/balastan/aktan_min.png')}}" alt="" class="keremet">Керемет көч</h2>
-            <iframe width="100%" height="500px" src="https://www.youtube.com/embed/{{$firstMedia->getUrl()}}?rel=0&amp;controls=1&amp;showinfo=0" frameborder="0" controls=0 allowfullscreen></iframe>
-            <h3 class="homevideoname">{{ $firstMedia->getName() }}</h3>
-            @endif
-         </div>
-         <div class="col-md-4">
-            <div class="right">
-               <a href="">
-                  <h2 class="cat">Билгим келет</h2>
-                  <img src="{{ asset('images/channels/balastan/keremet.jpg')}}" alt=""/> 
-                  <h3 class="name">24.04.2016</h3>
-               </a>
-            </div> 
-            <div class="rightbottom">
-               <a href="">
-                  <h2 class="cat">Билгим келет</h2>
-                   <img src="{{ asset('images/channels/balastan/sun-kids.jpg')}}" alt=""/>
-                  <h3 class="name">24.04.2016</h3>
-               </a>
-            </div>                    
-         </div>
-      </div>
-   </div>
-   <!-- END Home video -->
-   <!-- BEGIN Programms slide -->
-   <div class="container-fluid programs-slide">
-      <div class="col-md-12">
-         <div class="kids-slide">
-            <div class="col-md-3">
-               <a href="#"><img src="{{ asset('images/channels/balastan/keremet.jpg')}}" alt=""/>
-                  <h3 class="programmcat">Керемет көч</h3>   
-               </a>
-            </div>
-            <div class="col-md-3">
-               <a href="#"><img src="{{ asset('images/channels/balastan/sun-kids.jpg')}}" alt=""/>
-                  <h3 class="programmcat">Күн балдары</h3>   
-               </a>
-            </div>
-            <div class="col-md-3">
-               <a href="#"><img src="{{ asset('images/channels/balastan/baaryn.png')}}" alt=""/>
-                  <h3 class="programmcat">Акыл ордо</h3>   
-               </a>
-            </div>
-            <div class="col-md-3">
-               <a href="#"><img src="{{ asset('images/channels/balastan/birge2.png')}}" alt=""/>
-                  <h3 class="programmcat">Бирге окуйбуз</h3>   
-               </a>
-            </div>
-            <div class="col-md-3">
-               <a href="#"><img src="{{ asset('images/channels/balastan/manastaanuu.jpg')}}" alt=""/>
-                  <h3 class="programmcat">Манас Таануу</h3>   
-               </a>
-            </div>
-            <div class="col-md-3">
-               <a href="#"><img src="{{ asset('images/channels/balastan/taalimtay.png')}}" alt=""/>
-                  <h3 class="programmcat">Таалимтай</h3>   
-               </a>
+   <div class="container-fluid kidsmainbg">
+      <div class="container">
+         <div class="row">
+            <div class="col-md-12">
+               <nav class="kidstopheader">
+                  <ul>
+                     <li><a class="active" href="{{ route('balastan.home') }}"><i class="fa fa-home i1"></i>{{ trans('radiopages.Home') }}</a></li>
+                     <li><a href="{{ route('balastan.about') }}"><i class="fa fa-hand-o-up i2"></i>{{ trans('radiopages.BAbout') }}</a></li>
+                     <li><a href="{{ route('balastan.world') }}"><i class="fa fa-star i3"></i>{{ trans('radiopages.Bworld') }}</a></li>
+                     <li><a href="{{ route('balastan.video') }}"><i class="fa fa-play i4 "></i>Видео</a></li>
+                     <li><a href="{{ route('balastan.allphotos') }}"><i class="fa fa-picture-o i5"></i>{{ trans('radiopages.Photos') }}</a></li>
+                     <li><a href="#"><i class="fa fa-search i6"></i></a></li>
+                  </ul>
+               </nav>
             </div>
          </div>
-      </div>
-   </div>
-   <!-- END Programms slide -->
-   <div class="container">
-      <div class="row">
-         <div class="col-md-12 kidsvblock text-center">
-            <div class="panel panel-default">
-               <div class="panel-heading">
-                  <h3 class="panel-title"><span>Видео</span></h3>
+         <div class="row firsteye">
+            <div class="col-md-8 homevideo">
+               @if($firstMedia)
+               <h2 class="homevideocat"><img src="{{asset('images/channels/balastan/aktan_min.png')}}" alt="" class="keremet">Керемет көч</h2>
+               <iframe width="100%" height="500px" src="https://www.youtube.com/embed/{{$firstMedia->getUrl()}}?rel=0&amp;controls=1&amp;showinfo=0" frameborder="0" controls=0 allowfullscreen></iframe>
+               <h3 class="homevideoname">{{ $firstMedia->getName() }}</h3>
+               @endif
+            </div>
+            <div class="col-md-4">
+               <div class="right">
+                  <a href="">
+                     <h2 class="cat">Билгим келет</h2>
+                     <img src="{{ asset('images/channels/balastan/keremet.jpg')}}" alt=""/>
+                     <div class="play">
+                        <iframe src="https://www.youtube.com/embed/{{$firstMedia->getUrl()}}?rel=0&amp;controls=1&amp;showinfo=0" frameborder="0" controls=0 allowfullscreen></iframe>
+                     </div>
+                     <h3 class="name">24.04.2016</h3>
+                  </a>
                </div>
-               <div class="panel-body">
-                  <div class="row ">
-                     @if($medias != null)
-                     @foreach($medias as $media)
-                     <div class="col-md-4 col-sm-6">
-                        <div class="kidsvideo kidsboard">
-                           <a href="{{ route('balastan.video', $media) }}">
-                           <img width="100%" height="180px" src="http://img.youtube.com/vi/{{ $media->getUrl()}}/mqdefault.jpg" alt="" />                       
-                           </a>
-                        </div>
-                        <h3>{{ $media->getName() }}</h3>
+               <div class="rightbottom">
+                  <a href="">
+                     <h2 class="cat">Күн балдары</h2>
+                     <img src="{{ asset('images/channels/balastan/sun-kids.jpg')}}" alt=""/>
+                     <div class="play">
+                        <iframe src="https://www.youtube.com/embed/{{$firstMedia->getUrl()}}?rel=0&amp;controls=1&amp;showinfo=0" frameborder="0" controls=0 allowfullscreen></iframe>
                      </div>
-                     @endforeach
-                     @else
-                     <div class="col-md-4 col-sm-6">
-                        <div class="kidsvideo kidsboard">
-                           <a href="#">
-                           <img width="100%" height="180px" src="http://img.youtube.com/vi/rYEDA3JcQqw/mqdefault.jpg" alt="" />                  
-                           </a>
-                        </div>
-                        <h3>adele rolling in the deep</h3>
-                     </div>
-                     @endif
+                     <h3 class="name">31.02.2016</h3>
+                  </a>
+               </div>
+            </div>
+         </div>
+         <!-- END Home video -->
+         <!-- BEGIN Programms slide -->
+      </div>
+   </div>
+   <div class="container-fluid kidsslider">
+      <div class="container">
+         <div class="row">   
+            <div class="col-md-12 programs-slide">
+               <div class="kids-slide">
+                  <div class="col-md-3">
+                     <a href="#">
+                        <img src="{{ asset('images/channels/balastan/keremet.jpg')}}" alt=""/>
+                        <h3 class="programmcat">Керемет көч</h3>
+                     </a>
+                  </div>
+                  <div class="col-md-3">
+                     <a href="#">
+                        <img src="{{ asset('images/channels/balastan/sun-kids.jpg')}}" alt=""/>
+                        <h3 class="programmcat">Күн балдары</h3>
+                     </a>
+                  </div>
+                  <div class="col-md-3">
+                     <a href="#">
+                        <img src="{{ asset('images/channels/balastan/baaryn.png')}}" alt=""/>
+                        <h3 class="programmcat">Акыл ордо</h3>
+                     </a>
+                  </div>
+                  <div class="col-md-3">
+                     <a href="#">
+                        <img src="{{ asset('images/channels/balastan/birge2.png')}}" alt=""/>
+                        <h3 class="programmcat">Бирге окуйбуз</h3>
+                     </a>
+                  </div>
+                  <div class="col-md-3">
+                     <a href="#">
+                        <img src="{{ asset('images/channels/balastan/manastaanuu.jpg')}}" alt=""/>
+                        <h3 class="programmcat">Манас Таануу</h3>
+                     </a>
+                  </div>
+                  <div class="col-md-3">
+                     <a href="#">
+                        <img src="{{ asset('images/channels/balastan/taalimtay.png')}}" alt=""/>
+                        <h3 class="programmcat">Таалимтай</h3>
+                     </a>
                   </div>
                </div>
             </div>
          </div>
-      </div>
-      <div class="row" style="margin-bottom: 40px;">
-         <div class="col-md-12 kidsvblock kidgallery">
-            <div class="panel panel-default panel-carousel ">
-               <div class="panel-heading">
-                  <h3 class="panel-title"><span>{{ trans('radiopages.Photos') }}</span></h3>
+      </div>        
+   </div>
+   <!-- END Programms slide --> 
+
+   <!-- BEGIN NEW videos -->
+   <div class="container-fluid newtopvideos">
+      <div class="container">   
+         <div class="row">
+            <div class="col-md-6 newkidvideos">
+               <div class="title">
+                  <h3>Жаңы видеолор</h3>
                </div>
-               <div class="panel-body" style="margin-top:20px;">
-                  <div class="col-md-12 broadimages">
-                     <div class="carousel-slickkid">
-                        @if($photoGalleries)
-                        @foreach($photoGalleries as $photoGallery)
-                        <div>
-                           <a href="{{ route('balastan.photos', $photoGallery) }}">
-                           <img src="{{ asset($photoGallery->status) }}" alt=""/>
-                           </a>
-                           <div class="desc">
-                              <a href="{{route('balastan.photos', $photoGallery)}}">{{ $photoGallery->getName() }}</a>
-                           </div>
-                        </div>
-                        @endforeach
-                        @else
-                        <div class="col-md-4">
-                           <a href="#">
-                              <img src="" alt=""/><span>aba</span>
-                              <div class="overlay"></div>
-                              <i class="fa fa-camera"></i>
-                           </a>
-                        </div>
-                        @endif
-                     </div>
+               <div class="row">
+                  <div class="col-md-12">
+                     <a href="">
+                        <h2 class="cat">Манас таануу</h2>
+                        <img class="videomain" width="100%" height="340px" src="http://img.youtube.com/vi/rYEDA3JcQqw/mqdefault.jpg" alt="" />
+                        <h3 class="name">Биринчи чыгарылыш</h3>
+                     </a>
+                  </div>
+                  <div class="col-md-6">
+                     <a href="">
+                        <h2 class="cat">Манас таануу</h2>
+                        <img class="video" width="100%" height="180px" src="http://img.youtube.com/vi/rYEDA3JcQqw/mqdefault.jpg" alt="" />
+                        <h3 class="name">Биринчи чыгарылыш</h3>
+                     </a>
+                  </div>
+                  <div class="col-md-6">
+                     <a href="">
+                        <h2 class="cat">Манас таануу</h2>
+                        <img class="video" width="100%" height="180px" src="http://img.youtube.com/vi/rYEDA3JcQqw/mqdefault.jpg" alt="" />
+                        <h3 class="name">Биринчи чыгарылыш</h3>
+                     </a>
+                  </div>
+                  <div class="col-md-6">
+                     <a href="">
+                        <h2 class="cat">Манас таануу</h2>
+                        <img class="video" width="100%" height="180px" src="http://img.youtube.com/vi/rYEDA3JcQqw/mqdefault.jpg" alt="" />
+                        <h3 class="name">Биринчи чыгарылыш</h3>
+                     </a>
+                  </div>
+                  <div class="col-md-6">
+                     <a href="">
+                        <h2 class="cat">Манас таануу</h2>
+                        <img class="video" width="100%" height="180px" src="http://img.youtube.com/vi/rYEDA3JcQqw/mqdefault.jpg" alt="" />
+                        <h3 class="name">Биринчи чыгарылыш</h3>
+                     </a>
+                  </div>
+               </div>
+            </div>
+            <div class="col-md-6 newkidvideos">
+               <div class="title2">
+                  <h3>Популярдуу видеолор</h3>
+               </div>
+               <div class="row">
+                  <div class="col-md-12">
+                     <a href="">
+                        <h2 class="cat">Манас таануу</h2>
+                        <img class="videomain" width="100%" height="340px" src="http://img.youtube.com/vi/rYEDA3JcQqw/mqdefault.jpg" alt="" />
+                        <h3 class="name">Биринчи чыгарылыш</h3>
+                     </a>
+                  </div>            
+                  <div class="col-md-6">
+                     <a href="">
+                        <h2 class="cat">Манас таануу</h2>
+                        <img class="video" width="100%" height="180px" src="http://img.youtube.com/vi/rYEDA3JcQqw/mqdefault.jpg" alt="" />
+                        <h3 class="name">Биринчи чыгарылыш</h3>
+                     </a>
+                  </div>
+                  <div class="col-md-6">
+                     <a href="">
+                        <h2 class="cat">Манас таануу</h2>
+                        <img class="video" width="100%" height="180px" src="http://img.youtube.com/vi/rYEDA3JcQqw/mqdefault.jpg" alt="" />
+                        <h3 class="name">Биринчи чыгарылыш</h3>
+                     </a>
+                  </div>
+                  <div class="col-md-6">
+                     <a href="">
+                        <h2 class="cat">Манас таануу</h2>
+                        <img class="video" width="100%" height="180px" src="http://img.youtube.com/vi/rYEDA3JcQqw/mqdefault.jpg" alt="" />
+                        <h3 class="name">Биринчи чыгарылыш</h3>
+                     </a>
+                  </div>
+                  <div class="col-md-6">
+                     <a href="">
+                        <h2 class="cat">Манас таануу</h2>
+                        <img class="video" width="100%" height="180px" src="http://img.youtube.com/vi/rYEDA3JcQqw/mqdefault.jpg" alt="" />
+                        <h3 class="name">Биринчи чыгарылыш</h3>
+                     </a>
                   </div>
                </div>
             </div>
          </div>
+      </div>     
+   </div>
+   <!-- END NEW videos -->
+
+   <!-- BEGIN Photogalleries -->
+   <div class="container-fluid kidsgallery">
+      <div class="container">
+         <div class="row">
+            <div class="col-md-12">
+               <div class="title">
+                  <h3>Фотогалерея</h3>
+                  <h2 class="kidsright pull-right">Все фото</h2>
+               </div>
+            </div>
+            <div class="col-md-4">
+               <img class="cover" src="http://img.youtube.com/vi/rYEDA3JcQqw/mqdefault.jpg" alt="">
+               <h3 class="name"></h3>
+            </div>
+            <div class="col-md-4">
+               <img class="cover" src="http://img.youtube.com/vi/rYEDA3JcQqw/mqdefault.jpg" alt="">
+               <h3 class="name"></h3>
+            </div>
+            <div class="col-md-4">
+               <img class="cover" src="http://img.youtube.com/vi/rYEDA3JcQqw/mqdefault.jpg" alt="">
+               <h3 class="name"></h3>
+            </div>
+            <div class="col-md-4">
+               <img class="cover" src="http://img.youtube.com/vi/rYEDA3JcQqw/mqdefault.jpg" alt="">
+               <h3 class="name"></h3>
+            </div>
+            <div class="col-md-4">
+               <img class="cover" src="http://img.youtube.com/vi/rYEDA3JcQqw/mqdefault.jpg" alt="">
+               <h3 class="name"></h3>
+            </div>
+            <div class="col-md-4">
+               <img class="cover" src="http://img.youtube.com/vi/rYEDA3JcQqw/mqdefault.jpg" alt="">
+               <h3 class="name"></h3>
+            </div>
+         </div>
       </div>
    </div>
+   <!-- END Photogalleries -->
+   
    @stop
    @section('footerscript2')
    <script src="{{ asset('js/jquery-1.11.2.min.js')}}"></script>
