607b1314092316c947594d7ff2e732818a8b7149
Aibek Kendirbaev <kendirbaev.aibek@gmail.com>
2016-01-29 16:18:01 +0600

    last update

--- app/Acme/Http/Front/routesBeta.php
+++ app/Acme/Http/Front/routesBeta.php
@@ -101,8 +101,7 @@ Route::group(['domain' => 'beta.ktrk.kg', 'prefix' => '/birinchi', 'namespace' =
     Route::get('/photos/{gallery}', ['as' => 'birinchi.photos',   'uses' => 'BirinchiController@Gallery']);
     Route::get('/allphotos',['as' => 'birinchi.allphotos','uses'=> 'BirinchiController@allphotos'] );
     Route::get('/broadcastsprogramm', ['as' => 'birinchi.broadcastsprogramm',   'uses' => 'BirinchiController@broadcastsprogramm']);
-
-    Route::get('/vertex',['as'=>'birinchi.vertex', 'uses'=>'BirinchiController@vertex']);
+    Route::get('/category/{category}', ['as' => 'birinchi.category',   'uses' => 'BirinchiController@categoryPage']);
 
 });
 
