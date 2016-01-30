9498b9cc27915c32bb476c2f58ebb36e00aa4e06
Eldos Kadyrov <eldos.kadyrov@gmail.com>
2016-01-30 13:11:19 +0600

    Birinchi forgotten remove copy paste

--- app/Acme/Http/Front/Views/channel/birinchi/news.blade.php
+++ app/Acme/Http/Front/Views/channel/birinchi/news.blade.php
@@ -129,10 +129,10 @@
                               <div class="media-body">
                                   <div class="extra">
                                       <span class="datetime">{{ $post->getDay() }} {{ $post->getMonthRu() }}</span>
-                                      <a class="cat" href="{{ route('front.category', $post->category) }}"><span>{{ $post->category('category_id')->first()->getTitle() }}</span></a>
+                                      <a class="cat" href="{{ route('birinchi.category', $post->category) }}"><span>{{ $post->category('category_id')->first()->getTitle() }}</span></a>
                                       <span class="views"><i class="fa fa-eye"></i>{{ $post->getViewed() }}</span>
                                   </div>
-                                  <a class="media-heading" href="{{ route('front.post', $post) }}">{{ $post->getTitleRuOrKg() }}</a>
+                                  <a class="media-heading" href="{{ route('birinchi.news', $post) }}">{{ $post->getTitleRuOrKg() }}</a>
                               </div>
 
                           </div>
