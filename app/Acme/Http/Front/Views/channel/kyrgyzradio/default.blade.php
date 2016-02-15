c1ad4ab35965f8ef548ad37e948ad72c109c32be
Eldos Kadyrov <eldos.kadyrov@gmail.com>
2016-02-15 19:23:10 +0600

    1000 and Quotes

--- app/Acme/Http/Front/Views/channel/kyrgyzradio/default.blade.php
+++ app/Acme/Http/Front/Views/channel/kyrgyzradio/default.blade.php
@@ -14,11 +14,15 @@
     <link rel="stylesheet" href="{{ asset('css/landing/main.css')}}">
     <link rel="stylesheet" href="{{ asset('css/articles.css') }}"/>
     <link rel="stylesheet" href="{{ asset('css/pages.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('slick/slick.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('slick/slick-theme.css') }}"/>
+
     <link rel="stylesheet" href="{{asset('css/landing/animate.min.css')}}"/>
     <link rel="stylesheet" href="{{asset('css/landing/owl.carousel.css')}}"/>
     <link rel="stylesheet" href="{{asset('css/landing/owl.transitions.css')}}"/>
     <link rel="stylesheet" href="{{asset('css/landing/prettyPhoto.css')}}"/>
     <link rel="stylesheet" href="{{asset('css/landing/responsive.css')}}"/>
+    
 
     <link rel="stylesheet" type="text/css" href="{{ asset('css/custombox.css') }}"/>
     <link rel="stylesheet"  href="{{ asset('css/lightslider.css') }}"/>
