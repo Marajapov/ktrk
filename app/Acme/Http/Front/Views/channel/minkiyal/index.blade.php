29b5fe8e35c066c53d9180a479fdcc132362625a
Eldos Kadyrov <eldos.kadyrov@gmail.com>
2016-02-18 14:22:26 +0600

    MKFM ver 4.10

--- app/Acme/Http/Front/Views/channel/minkiyal/index.blade.php
+++ app/Acme/Http/Front/Views/channel/minkiyal/index.blade.php
@@ -4,47 +4,46 @@
 @endsection
 @section('content')
 @include('Front::channel.minkiyal.nav')
-<div class="container">
-<nav class="navbar navbar-minheader">
+<nav id="main-menu" class="navbar navbar-minheader">
   <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">
-        <img src="{{asset('images/channels/min-kiyal-min.png')}}" alt="">
-        <span>Кыялдар орундалат</span>
-      </a>
+    <div class="container"> 
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">
+          <img src="{{asset('images/channels/min-kiyal-min.png')}}" alt="">        
+        </a>
+        <!-- <p class="navbar-text">Кыялдар орундалат</p> -->
+      </div>
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li><a href="#">Башкы</a></li>
+          <li><a href="#">Сүрөтбаяндар</a></li>
+          <li><a href="#">Биз жөнүндө</a></li>
+          <li><a href="#">Ди-Джейлер</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+                <div class="online">
+              <h3><i class="fa fa-microphone"></i>Түз эфир</h3>
+              <object width="65" height="40" data="http://ktrk.kg/online/uppod.swf">
+                 <param name="bgcolor" value="#ffffff" />
+                 <param name="allowFullScreen" value="false" />
+                 <param name="allowScriptAccess" value="false" />
+                 <param name="audio" value="http://ktrk.kg/online/uppod.swf" />
+                 <param name="flashvars" value="comment=KTRK&amp;st=03AEEZykN3Q3Q3QH3zkOk1mwXRWNvz3b1XAxYTtj1mzC5kA2uyb1kOk1mwXRWNvhzdwf9vwjzC5k=jQZi6jNf1kOk1mwXRWHNXzBGDY31X=jOkZ2sN6HsQfzC5kev0kttj1vL2NXzC5k1sQE3Q2GkTCGfFdGJTCHwXpvQXeWNhHm0X1hsNkTWwWTSUhTd0kNtj1JTWwf9vwnY31DX1CYLex5W=jaktjR1i6W0kbSal1C0V9Rv1XAuaTtj1tzC5kb42Qi63Gktj1ozfUrd&amp;file=http://212.112.96.232:8081" />
+              </object>
+           </div>
+        </ul>
+      </div><!-- /.navbar-collapse -->
     </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li><a href="#">Башкы</a></li>
-        <li><a href="#">Сүрөтбаяндар</a></li>
-        <li><a href="#">Биз жөнүндө</a></li>
-        <li><a href="#">Ди-Джейлер</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-              <div class="online">
-            <h3><i class="fa fa-microphone"></i>Түз эфир</h3>
-            <object width="65" height="40" data="http://ktrk.kg/online/uppod.swf">
-               <param name="bgcolor" value="#ffffff" />
-               <param name="allowFullScreen" value="false" />
-               <param name="allowScriptAccess" value="false" />
-               <param name="audio" value="http://ktrk.kg/online/uppod.swf" />
-               <param name="flashvars" value="comment=KTRK&amp;st=03AEEZykN3Q3Q3QH3zkOk1mwXRWNvz3b1XAxYTtj1mzC5kA2uyb1kOk1mwXRWNvhzdwf9vwjzC5k=jQZi6jNf1kOk1mwXRWHNXzBGDY31X=jOkZ2sN6HsQfzC5kev0kttj1vL2NXzC5k1sQE3Q2GkTCGfFdGJTCHwXpvQXeWNhHm0X1hsNkTWwWTSUhTd0kNtj1JTWwf9vwnY31DX1CYLex5W=jaktjR1i6W0kbSal1C0V9Rv1XAuaTtj1tzC5kb42Qi63Gktj1ozfUrd&amp;file=http://212.112.96.232:8081" />
-            </object>
-         </div>
-      </ul>
-    </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
-</div>
 <div class="container">
    <div class="shows-slider slidermin">
       <div class="slider-overlay"></div>
