0babc27c219f5dbbb95de350a7500ac5bd74f0b8
Eldos Kadyrov <eldos.kadyrov@gmail.com>
2016-05-28 13:34:25 +0600

    Birinchi ukturrular block edit

--- app/Acme/Http/Front/Views/channel/birinchi/index.blade.php
+++ app/Acme/Http/Front/Views/channel/birinchi/index.blade.php
@@ -128,32 +128,32 @@
                     </div>
                 @endforeach
 
-                <div class="col-md-12 shows">
+                <div class="col-md-12 shows birinchigallery">
                     <div class="row">
                         <div class="col-md-12">
                             <h3 class="titleshows">{{ trans('radiopages.Peredachi') }}</h3>
-                            <!-- <h3 class="titleshowsright"><a href="{{ route('birinchi.projects') }}">{{ trans('radiopages.AllPereadachi') }}<i class="fa fa-arrow-right"></i></a></h3> -->
+                            <h3 class="titleshowsright"><a href="{{ route('birinchi.projects') }}">{{ trans('radiopages.AllPereadachi') }}<i class="fa fa-arrow-right"></i></a></h3>
                         </div>
 
                         @if($projectAll)
-                            @foreach($projectAll as $post)                                                   
-                                    <div class="blocknews2 col-md-4 col-sm-4 col-xs-12">
-                                        <article>                                                             
-                                            <div class="hovertext">
-                                               <div class="extra">
-                                                  <span class="datetime">{{ $post->getDay() }} , {{ $post->getMonthRu() }}, {{ $post->getTime()}}</span>
-                                                  <span class="views"><i class="fa fa-eye"></i>{{ $post->getViewed() }}</span>
-                                               </div>
-                                               <a href="{{ route('birinchi.show', $post) }}">
-                                                  <img class="media-object" src="{{ asset($post->getFile()) }}" alt="...">
-                                                  <div class="desc"><span>{!! mb_substr($post->getContent(), 0, 290, "UTF-8") !!}</span></div>
+                            @foreach($projectAll as $post)                                                  
+        
+                                        <div class="blocknews col-md-4 col-sm-4 col-xs-12">
+                                            <article class="ukturuular">
+                                                <a href="{{ route('birinchi.show', $post) }}" class="image-link">
+                                                    <img src="@if(!($post->getFile()))images/live_bg.png @else {{ asset($post->getFile()) }} @endif">
+                                                    <div class="card-info">
+                                                        <span class="date ">{{ $post->getDay() }} , {{ $post->getMonthRu() }}, {{ $post->getTime()}}</span>
+                                                        <span class="view"><i class="fa fa-eye"></i>{{ $post->getViewed() }}</span>
+                                                    </div>
                                                 </a>
-                                            </div>
-                                            <h3 class="name headline">                                               
-                                                <a class="media-heading " href="{{ route('birinchi.show', $post) }}">{{ $post->getTitleRuOrKg() }}</a>                                         
-                                           </h3>
-                                        </article>
-                                    </div>                
+                                                <h3 class="name headline">
+                                                    <a href="{{ route('birinchi.show', $post) }}" title="">
+                                                        {{ $post->getTitleRuOrKg() }}
+                                                    </a>
+                                                </h3>
+                                            </article>
+                                        </div>              
                              
                             @endforeach
                         @endif
