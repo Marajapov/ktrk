b4c3b79c713c5cc128c221ef22f487587656fc9b
Eldos Kadyrov <eldos.kadyrov@gmail.com>
2016-06-07 17:41:14 +0600

    Logo add

--- app/Acme/Http/Front/Views/channel/birinchi/index.blade.php
+++ app/Acme/Http/Front/Views/channel/birinchi/index.blade.php
@@ -162,8 +162,8 @@
                 <div class="col-md-4">
                     <div class="row">
                         <div class="col-md-12 shows" >
-                            <h3 class="titleshows">Аудиоуктуруулар</h3>
-                            <iframe width="100%" height="490" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/229824839&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
+                            <h3 class="titleshows">Аудиоуктуруулар</h3>                            
+                            <iframe width="100%" height="490" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/229824839&amp;color=00aabb&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
                         </div>
 
                     </div>
